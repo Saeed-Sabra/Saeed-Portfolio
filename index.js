74116836d0bc9c472b788f07c22400f7d5e53810
Saeed Sabra <saeedsabra565@gmail.com>
2023-07-10 17:34:42 +0300

    responsive website

--- index.js
+++ index.js
@@ -11,3 +11,11 @@ function openTab(tabName) {
   event.currentTarget.classList.add("active-link");
   document.getElementById(tabName).classList.add("active-tab");
 }
+
+var sidemenu = document.getElementById("sidemenu");
+function openMenu() {
+  sidemenu.style.right = "0";
+}
+function closeMenu() {
+  sidemenu.style.right = "-200px";
+}
