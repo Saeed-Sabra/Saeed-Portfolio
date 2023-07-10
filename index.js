c31fca0f21ffdb0e27f803409aa8d134e2bc9095
Saeed Sabra <saeedsabra565@gmail.com>
2023-07-10 17:38:28 +0300

    Send contact form  data on google sheets

--- index.js
+++ index.js
@@ -19,3 +19,28 @@ function openMenu() {
 function closeMenu() {
   sidemenu.style.right = "-200px";
 }
+
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbzxUZ9-2qviBi-jCt7bDCAkStOaEQKbfM3bnjZQcBASozizvZdRRMt6LWtXavFwL1ce/exec";
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "Message sent Successfully";
+
+      setTimeout(() => {
+        msg.innerHTML = "";
+      }, 5000);
+      form.reset();
+    })
+    .catch((error) => {
+      msg.innerHTML = "Something went wrong!";
+
+      setTimeout(() => {
+        msg.innerHTML = "";
+      }, 5000);
+    });
+});
